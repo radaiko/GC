840ad7423822bd5d781c255d35d793103d2f817a
radaiko <a.radlingmayr@icloud.com>
2025-10-15 18:26:18 +0200

    first implementation of settings

--- GC/GC/ViewModels/ViewModelBase.cs
+++
@@ -1,5 +0,0 @@
-﻿using ReactiveUI;
-
-namespace GC.ViewModels;
-
-public class ViewModelBase : ReactiveObject { }
--- GC/GC/Views/MainView.axaml.cs
+++
@@ -1,9 +0,0 @@
-using Avalonia.Controls;
-
-namespace GC.Views;
-
-public partial class MainView : UserControl {
-  public MainView() {
-    InitializeComponent();
-  }
-}
